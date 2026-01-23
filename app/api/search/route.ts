2eb1a43949f6431370ae97d72acdd0ecb4a793de
donaldxdonald <mzhlovefama@gmail.com>
2026-01-24 00:02:22 +0800

    fix: dynamic search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,7 +3,7 @@ import { createTokenizer } from '@orama/tokenizers/mandarin'
 import { stopwords as mandarinStopwords } from '@orama/stopwords/mandarin'
 import { weeklySource } from '../../source'
 
-export const { staticGET: GET } = createFromSource(weeklySource, {
+export const { GET } = createFromSource(weeklySource, {
   tokenizer: createTokenizer({
     stopWords: mandarinStopwords,
     language: 'mandarin',
